24df79b2cf62e320f638ce579dbc0671a857e0e1
KKamagui-0 <kkamagui.0@gamil.com>
2024-08-23 17:22:31 +0900

    jebal

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # SQLAlchemy 설정
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///joleeus.db'
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY','default_secret_key')
 db = SQLAlchemy(app)
 
 # CSRF 보호 활성화
